fbff7d32638be4fbcd27f7f25cd9ace5ff8ed325
Kumar Sambhav <sambhav26k@gmail.com>
2024-04-10 13:18:14 +0530

    package modifications

---
+++ app/src/main/java/in/iot/lab/androidui_uxlibrary/animations/Dummy.kt
@@ -0,0 +1,5 @@
+package `in`.iot.lab.androidui_uxlibrary.animations
+
+class Dummy {
+//    TODO:Delete this file after adding your code
+}
---
+++ app/src/main/java/in/iot/lab/androidui_uxlibrary/canvas/Dummy.kt
@@ -0,0 +1,5 @@
+package `in`.iot.lab.androidui_uxlibrary.canvas
+
+class Dummy {
+//    TODO:Delete this file after adding the code
+}
